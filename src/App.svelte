5cb449b00ec0708248cbf6f5837e5e9f082cf9d8
remus432 <brosdev1@gmail.com>
2022-01-04 13:43:42 +0200

    design files

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Card from "./components/Card.svelte"
+
+	let cars = [
+		{
+			type: "Sedans",
+			description: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+			icon: "/images/icon-sedans.svg",
+			color: "hsl(31, 77%, 52%)"
+		},
+		{
+			type: "Suvs",
+			description: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+			icon: "/images/icon-suvs.svg",
+			color: "hsl(184, 100%, 22%)"
+		},
+		{
+			type: "Luxury",
+			description: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+			icon: "/images/icon-luxury.svg",
+			color: "hsl(179, 100%, 13%)"
+		}
+	]
+</script>
+
+<main class="wrapper">
+	{#each cars as car, i}
+		<Card
+			carIcon={car.icon}
+			carType={car.type}
+			carDescription={car.description}
+			color={car.color}
+			index={i} />
+	{/each}
+</main>
+
+<style lang="scss">
+	@media screen and (min-width: 768px) {
+		.wrapper {
+			display: flex;
+			padding: 15rem 25rem;
+		}
+	}
+</style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,81 @@
+<script>
+  export let carIcon
+  export let carType
+  export let carDescription
+  export let color
+  export let index
+</script>
+
+<div style="background-color: {color}" class="card card--{index+1}">
+  <img src={carIcon} alt="Car Icon" class="card__icon">
+  <h2 class="card__headline">{carType}</h2>
+  <p class="card__description">{carDescription}</p>
+  <button style="color: {color}" class="card__btn">Learn More</button>
+</div>
+
+<style lang="scss">
+  .card {
+    padding: 4.8rem;
+
+    & > *:first-child {
+      margin-bottom: 3.5rem;
+    }
+
+    & > *:not(:first-child) {
+      margin-bottom: 2.5rem;
+    }
+
+    &__headline {
+      color: hsl(0, 0%, 95%);
+      font-family: "Big Shoulders Display";
+      font-size: 4rem;
+      text-transform: uppercase;
+    }
+
+    &__description {
+      color: hsla(0, 0%, 100%, 0.75);
+    }
+
+    &__btn {
+      background-color: hsl(0, 0%, 95%);
+      border: 2px solid transparent;
+      border-radius: 2.5rem;
+      font-family: "Lexend Deca";
+      padding: 1.1rem 3.15rem;
+    }
+
+    @media screen and (min-width: 768px) {
+      &--1 {
+        animation: fadeInDown 1s backwards ease-in-out;
+      }
+
+      &--2 {
+        animation: fadeInDown 1s .5s backwards ease-in-out;
+      }
+
+      &--3 {
+        animation: fadeInDown 1s 1s backwards ease-in-out;
+      }
+
+      &:first-of-type {
+        border-top-left-radius: .8rem;
+        border-bottom-left-radius: .8rem;
+      }
+
+      &:last-of-type {
+        border-top-right-radius: .8rem;
+        border-bottom-right-radius: .8rem;
+      }
+
+      &__btn {
+        cursor: pointer;
+
+        &:hover {
+          background-color: transparent;
+          border: 2px solid hsl(0, 0%, 95%);
+          color: hsl(0, 0%, 95%)!important;
+        }
+      }
+    }
+  }
+</style>
